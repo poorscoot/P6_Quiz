a2fb6b4469f9c8226c78b67adc6ed831b293a0dc
poorscoot <poorscoot@hotmail.com>
2018-05-09 19:01:45 +0200

    Version 0.1

--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,7 @@ router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-
+router.get('/quizzes/randomplay',    	   quizController.randomPlay);
+router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomCheck);
 
 module.exports = router;
